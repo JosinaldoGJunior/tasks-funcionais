9a44030179f37e3fa1242e046d0fda4b7e9dc5d3
Josinaldo Júnior <josinaldo_jr@hotmail.com>
2021-01-14 10:06:05 -0300

    Atualização de IP x2

--- src/test/java/Prod/HealthCheckIT.java
+++ src/test/java/Prod/HealthCheckIT.java
@@ -16,9 +16,9 @@ public class HealthCheckIT {
     @Test
     public void healthCheck() throws MalformedURLException {
         DesiredCapabilities cap = DesiredCapabilities.chrome();
-        WebDriver driver = new RemoteWebDriver(new URL("http://10.0.0.103:4444/wd/hub"),cap);
+        WebDriver driver = new RemoteWebDriver(new URL("http://10.0.0.102:4444/wd/hub"),cap);
         try {
-            driver.navigate().to("http://10.0.0.103:9999/tasks/");
+            driver.navigate().to("http://10.0.0.102:9999/tasks/");
             driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
             String version = driver.findElement(By.id("version")).getText();
             Assert.assertTrue(version.startsWith("build"));
