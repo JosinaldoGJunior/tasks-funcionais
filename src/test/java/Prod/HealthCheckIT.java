618200d63f10dfb0abda6b93bb42d59eb64a5876
Josinaldo Júnior <josinaldo_jr@hotmail.com>
2021-03-01 16:15:58 -0300

    Atualização de IP x4

--- src/test/java/Prod/HealthCheckIT.java
+++ src/test/java/Prod/HealthCheckIT.java
@@ -16,9 +16,9 @@ public class HealthCheckIT {
     @Test
     public void healthCheck() throws MalformedURLException {
         DesiredCapabilities cap = DesiredCapabilities.chrome();
-        WebDriver driver = new RemoteWebDriver(new URL("http://10.0.0.105:4444/wd/hub"),cap);
+        WebDriver driver = new RemoteWebDriver(new URL("http://192.168.15.9:4444/wd/hub"),cap);
         try {
-            driver.navigate().to("http://10.0.0.105:9999/tasks/");
+            driver.navigate().to("http://192.168.15.9:9999/tasks/");
             driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
             String version = driver.findElement(By.id("version")).getText();
             Assert.assertTrue(version.startsWith("build"));
--- src/test/java/TasksTest.java
+++ src/test/java/TasksTest.java
@@ -14,8 +14,8 @@ public class TasksTest {
  public WebDriver acessarAplicacao() throws MalformedURLException {
     // WebDriver driver = new ChromeDriver();
      DesiredCapabilities cap = DesiredCapabilities.chrome();
-     WebDriver driver = new RemoteWebDriver(new URL("http://10.0.0.105:4444/wd/hub"),cap);
-     driver.navigate().to("http://10.0.0.105:8001/tasks/");
+     WebDriver driver = new RemoteWebDriver(new URL("http://192.168.15.9:4444/wd/hub"),cap);
+     driver.navigate().to("http://192.168.15.9:8001/tasks/");
      driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
      return driver;
  }
